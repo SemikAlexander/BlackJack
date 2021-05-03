95f8e4558e8ebb32448721f61b909c8d93437c5f
SemikAlexander <36263835+SemikAlexander@users.noreply.github.com>
2021-05-03 22:35:58 +0300

    Make some logic for game activity

--- app/src/main/java/com/example/blackjack/GameActivity.kt
+++ app/src/main/java/com/example/blackjack/GameActivity.kt
@@ -1,11 +1,16 @@
 package com.example.blackjack
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R.attr.*
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
 import com.example.blackjack.databinding.ActivityGameBinding
+
 
 class GameActivity : AppCompatActivity() {
     lateinit var binding: ActivityGameBinding
+    var count: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,7 +26,26 @@ class GameActivity : AppCompatActivity() {
             playerPoints.text = "0"
             dealerPoints.text = "0"
 
-            deal
+            playerCardsField.removeAllViews()
+
+            hitButton.setOnClickListener {
+
+                val lp = LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.WRAP_CONTENT,
+                        LinearLayout.LayoutParams.WRAP_CONTENT
+                )
+
+                val iv = ImageView(this@GameActivity)
+                iv.setBackgroundResource(R.drawable.queen_of_hearts)
+
+                if (count != 0) {
+                    lp.setMargins(-240, 0, 0, 0)
+                    iv.layoutParams = lp
+                }
+
+                playerCardsField.addView(iv)
+                count++
+            }
         }
     }
 
